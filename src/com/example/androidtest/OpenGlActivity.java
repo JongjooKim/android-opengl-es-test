44361c1cdc38c2ecd51bc4dba0bdd825ad90ce5e
Jongjoo Kim <powerqn@gmail.com>
2014-03-31 00:09:33 +0900

    corrected touch points

--- src/com/example/androidtest/OpenGlActivity.java
+++ src/com/example/androidtest/OpenGlActivity.java
@@ -1,20 +1,26 @@
 package com.example.androidtest;
 
 import javax.microedition.khronos.opengles.GL;
+
 import com.example.android.apis.graphics.spritetext.MatrixTrackingGL;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.Window;
 
 public class OpenGlActivity extends Activity {
 	private final String LOG_TAG = "OpenGLActivity";
 	
+	private int statusBarHeight = 0;
+	private int titleBarHeight = 0;
+	
 	private GLSurfaceView glSurfaceView;
 	private RenderBase mCurrentRender = null;
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
@@ -68,6 +74,18 @@ public class OpenGlActivity extends Activity {
 		});
 	}	
 	
+	
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		
+		if(hasFocus) {
+			statusBarHeight = getStatusBarHeight();
+			titleBarHeight = getTitleBarHeight();
+		}
+	}
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		// Log.d(LOG_TAG, "onTouchEvent() : event : " + event);
@@ -76,11 +94,28 @@ public class OpenGlActivity extends Activity {
 			MotionEvent event2 = MotionEvent.obtain(event);
 			// TODO converts (x, y) to (x,y) on viewport
 			// TODO subtracts height of title bar and status bar
+			event2.setLocation(event2.getX(), event2.getY() - statusBarHeight - titleBarHeight);	
 			mCurrentRender.handleTouchEvent(event2);
 		}
 		
 		return super.onTouchEvent(event);
 	}
+	
+	public int getTitleBarHeight() {
+	    int viewTop = getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+	    return (viewTop - getStatusBarHeight());
+	}
+	
+	public int getStatusBarHeight() {
+		int result = 0;
+       int resourceId = getResources().getIdentifier("status_bar_height", 
+    		   "dimen", "android");
+       if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+       
+       return result;
+    }
 
 	@Override
 	protected void onResume() {
