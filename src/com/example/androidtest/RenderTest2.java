c74215bf598630910945ab6fe1ee43691b314c52
Jongjoo Kim <powerqn@gmail.com>
2014-04-09 01:15:28 +0900

    trivial

--- src/com/example/androidtest/RenderTest2.java
+++ src/com/example/androidtest/RenderTest2.java
@@ -30,6 +30,12 @@ public class RenderTest2 extends RenderBase {
 	
 	private PointF eventPointF;
 	private int width, height;
+	
+	private int MVPMatrixHandle;
+	private int positionHandle;
+	private int colorHandle;
+	
+	private int strideBytes = 7 * 4;
 		
 	private float[] squareVerticesData = {
 			0.0f, 0.0f, 0.0f,
@@ -102,61 +108,42 @@ public class RenderTest2 extends RenderBase {
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		// Log.d(LOG_TAG, "onDrawFrame() : gl : " + gl);
-		
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);		
-		
-		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-			
-		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, lineBuffer);
-		gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);		
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		
 		// fills upper-left field
-		gl.glPushMatrix();
 		for(int i = 1; i <= 10; i++) {			
 			for(int j = 0; j < 10; j++) {
-				gl.glPushMatrix();
-				gl.glTranslatef(-0.1f * i, +0.1f * j, 0.0f);
-				gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 4);
-				gl.glPopMatrix();
+				Matrix.setIdentityM(modelMatrix, 0);
+				Matrix.translateM(modelMatrix, 0, -0.1f * i, +0.1f * j, 0.0f);	
+				drawSquare(squareVerticesBuffer);							
 			}
-		}
-		gl.glPopMatrix();
+		}		
 		// fills upper-right field
-		gl.glPushMatrix();
 		for(int i = 0; i <= 10; i++) {			
 			for(int j = 0; j < 10; j++) {
 				gl.glPushMatrix();
 				gl.glTranslatef(+0.1f * i, +0.1f * j, 0.0f);
 				gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 4);
-				gl.glPopMatrix();
 			}
 		}
-		gl.glPopMatrix();
 		// fills lower-left field
-		gl.glPushMatrix();
 		for(int i = 1; i <= 10; i++) {			
 			for(int j = 1; j <= 10; j++) {
 				gl.glPushMatrix();
 				gl.glTranslatef(-0.1f * i, -0.1f * j, 0.0f);
 				gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 4);
-				gl.glPopMatrix();
 			}
 		}
-		gl.glPopMatrix();
 		// fills lower-right field
-		gl.glPushMatrix();
 		for(int i = 0; i < 10; i++) {			
 			for(int j = 1; j <= 10; j++) {
 				gl.glPushMatrix();
 				gl.glTranslatef(+0.1f * i, -0.1f * j, 0.0f);
-				gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 4);
-				gl.glPopMatrix();
+				gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 4);				
 			}
 		}
-		gl.glPopMatrix();
-
+		
+		
 		gl.glColorPointer(4, GL10.GL_FLOAT, 0, redColorBuffer);
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, originBuffer);
 		
@@ -178,6 +165,26 @@ public class RenderTest2 extends RenderBase {
 			gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
 		}
 	}
+	
+	private void drawSquare(FloatBuffer buffer) {
+		Log.d(LOG_TAG, "drawSquare() is called...");
+		
+		buffer.position(0);
+		GLES20.glVertexAttribPointer(positionHandle, 3, GLES20.GL_FLOAT, false, 
+				strideBytes, buffer);
+		GLES20.glEnableVertexAttribArray(positionHandle);
+		
+		buffer.position(3);
+		GLES20.glVertexAttribPointer(colorHandle, 0, GLES20.GL_FLOAT, false, 
+				strideBytes, buffer);
+		GLES20.glEnableVertexAttribArray(colorHandle);
+		
+		Matrix.multiplyMM(MVPMatrix, 0, viewMatrix, 0, modelMatrix, 0);
+		// Matrix.multiplyMM(MVPMatrix, 0, projectionMatrix, 0, MVPMatrix, 0);
+		
+		GLES20.glUniformMatrix4fv(MVPMatrixHandle, 1, false, MVPMatrix, 0);
+		GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -186,7 +193,7 @@ public class RenderTest2 extends RenderBase {
 		this.width = width;
 		this.height = height;
 		
-		gl.glViewport(0, 0, width, height);
+		GLES20.glViewport(0, 0, width, height);
 	}
 
 	@Override
@@ -199,7 +206,16 @@ public class RenderTest2 extends RenderBase {
 		squareVerticesBuffer = this.createFloatBuffer(squareVerticesData);
 		originVerticesBuffer = this.createFloatBuffer(originVerticesData);
 		
-		// blueColorBuffer = createFloatBuffer(blueCoor);		
+		int vertexShaderHandle = this.compileShader(GLES20.GL_VERTEX_SHADER, vertexShader);
+		int fragmentShaderHandle = this.compileShader(GLES20.GL_FRAGMENT_SHADER, fragmentShader);
+		int programHandle = this.createAndLinkProgram(vertexShaderHandle, fragmentShaderHandle, 
+				new String[]{"a_Position", "a_Color"});
+		
+		MVPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
+		positionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
+		colorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
+		
+		GLES20.glUseProgram(programHandle);
 	}
 	
 	@Override
